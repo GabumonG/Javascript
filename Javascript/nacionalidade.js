1fa75d4603e5fef9594fc51db3590dbe8af19c17
GabumonG <GabumonG@github.com>
2021-06-06 15:38:40 -0300

    COMMIT-06-06-2021
    Atualizando atividades de Javascript

---
+++ Javascript/nacionalidade.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+console.log(`Local de nascimento: ${pais}`)
+
+if(pais == 'Brasil'){
+    console.log(`Você é Brasileiro(a)`)
+}
+else{
+    console.log(`Você é estrangeiro`)
+}
