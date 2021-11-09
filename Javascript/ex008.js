a777a3bd1b1d8c50e3f15766fc0d903228e227af
Gabriela <gaby.gamergg@gmail.com>
2021-11-08 21:20:04 -0300

    Atualização Javascript 08-11-2021
    Finalização do módulo sobre DOM e início dos estudos relacionados a estruturas condicionais

---
+++ Javascript/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é: ${vel}km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade, será MULTADO!')
+}
+console.log('Dirija sempre com cuidado!')
